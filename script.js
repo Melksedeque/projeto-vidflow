86f30127c6c51580f8284ac195aed831c361d4aa
Melksedeque <freelancer@melksedeque.com.br>
2025-01-11 22:44:54 -0300

    Finalização da função de filtrar videos pela barra de busca segundo explicado na aula

--- script.js
+++ script.js
@@ -33,8 +33,24 @@ const barraDePesquisa = document.querySelector(".pesquisar__input")
 barraDePesquisa.addEventListener("input", filtrarPesquisa)
 
 function filtrarPesquisa() {
-    const videos = document.querySelector(".videos__item")
+    const videos = document.querySelectorAll(".videos__item")
 
-    if(barraDePesquisa.value != "") {}
-    else {}
+    if(barraDePesquisa.value) {
+        for(let video of videos) {
+            let titulo = video.querySelector(".titulo-video").textContent.toLowerCase()
+            let valorFiltro = barraDePesquisa.value.toLowerCase()
+
+            if(!titulo.includes(valorFiltro)) {
+                video.style.display = "none"
+            }
+            else {
+                video.style.display = "block"
+            }
+        }
+    }
+    else {
+        for(let video of videos) {
+            video.style.display = "block"
+        }
+    }
 }
