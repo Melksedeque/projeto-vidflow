d674d6a61027ad3b6d926545e28732471ea7c65c
Melksedeque <freelancer@melksedeque.com.br>
2025-01-12 01:03:36 -0300

    Aplicação de Prettier no código JS

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-const containerVideos = document.querySelector(".videos__container")
-const barraDePesquisa = document.querySelector(".pesquisar__input")
-const btnCategoria = document.querySelectorAll(".superior__item")
+const containerVideos = document.querySelector(".videos__container");
+const barraDePesquisa = document.querySelector(".pesquisar__input");
+const btnCategoria = document.querySelectorAll(".superior__item");
 
 async function buscarEMostrarVideos() {
-    try {
-        const busca = await fetch("http://localhost:3000/videos")
-        const videos = await busca.json()
+  try {
+    const busca = await fetch("http://localhost:3000/videos");
+    const videos = await busca.json();
 
-        videos.forEach((video) => {
-            if(video.categoria == "") {
-                throw new Error("Vídeo sem categoria!");
-            }
-            containerVideos.innerHTML += `
+    videos.forEach((video) => {
+      if (video.categoria == "") {
+        throw new Error("Vídeo sem categoria!");
+      }
+      containerVideos.innerHTML += `
                 <li class="videos__item">
                     <iframe src="${video.url}" title="${video.titulo}" frameborder="0" allowfullscreen></iframe>
                     <div class="descricao-video">
@@ -21,54 +21,60 @@ async function buscarEMostrarVideos() {
                         <p class="categoria" hidden>${video.categoria}</p>
                     </div>
                 </li>
-            `
-        })
-    }
-    catch(error) {
-        containerVideos.innerHTML = `<p>Houve um erro ao carregar os vídeos: <strong>${error}</strong></p>`
-    }
+            `;
+    });
+  } catch (error) {
+    containerVideos.innerHTML = `<p>Houve um erro ao carregar os vídeos: <strong>${error}</strong></p>`;
+  }
 }
 
-buscarEMostrarVideos()
+buscarEMostrarVideos();
 
 /**
  * Filtrar Vídeos pela Barra de Pesquisa
  */
-barraDePesquisa.addEventListener("input", filtrarPesquisa)
+barraDePesquisa.addEventListener("input", filtrarPesquisa);
 function filtrarPesquisa() {
-    const videos = document.querySelectorAll(".videos__item")
-    let valorFiltro = barraDePesquisa.value.toLowerCase()
-    
-    videos.forEach((video) => {
-        const titulo = video.querySelector(".titulo-video").textContent.toLowerCase()
-        video.style.display = titulo.includes(valorFiltro) ? "block" : "none"
-    })
+  const videos = document.querySelectorAll(".videos__item");
+  let valorFiltro = barraDePesquisa.value.toLowerCase();
+
+  videos.forEach((video) => {
+    const titulo = video
+      .querySelector(".titulo-video")
+      .textContent.toLowerCase();
+    video.style.display = titulo.includes(valorFiltro) ? "block" : "none";
+  });
 }
 
 /**
  * Filtrar Vídeos pelo Botão da respectiva Categoria
  */
 btnCategoria.forEach((botao) => {
-    const nomeCategoria = botao.getAttribute("name")
-    botao.addEventListener("click", () => {
-        filtrarPorCategoria(nomeCategoria)
-        definirBotaoAtivo(botao)
-    })
-})
+  const nomeCategoria = botao.getAttribute("name");
+  botao.addEventListener("click", () => {
+    filtrarPorCategoria(nomeCategoria);
+    definirBotaoAtivo(botao);
+  });
+});
 function filtrarPorCategoria(filtro) {
-    const videos = document.querySelectorAll(".videos__item")
-    const categoriaTodos = filtro.toLowerCase() === "tudo"
-    
-    videos.forEach((video) => {
-        const categoria = video.querySelector(".categoria").textContent.toLowerCase()
-        video.style.display = categoriaTodos || categoria.includes(filtro.toLowerCase()) ? "block" : "none"
-    })
+  const videos = document.querySelectorAll(".videos__item");
+  const categoriaTodos = filtro.toLowerCase() === "tudo";
+
+  videos.forEach((video) => {
+    const categoria = video
+      .querySelector(".categoria")
+      .textContent.toLowerCase();
+    video.style.display =
+      categoriaTodos || categoria.includes(filtro.toLowerCase())
+        ? "block"
+        : "none";
+  });
 }
 function definirBotaoAtivo(categoria) {
-    let categoriaAtiva = document.querySelector(".superior__item.ativo")
+  let categoriaAtiva = document.querySelector(".superior__item.ativo");
 
-    if(categoriaAtiva) {
-        categoriaAtiva.classList.remove("ativo")
-    }
-    categoria.classList.add("ativo")
+  if (categoriaAtiva) {
+    categoriaAtiva.classList.remove("ativo");
+  }
+  categoria.classList.add("ativo");
 }
